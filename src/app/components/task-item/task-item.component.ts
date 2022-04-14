ad76b339c86bab9c8c6f8918be4b72757ad2b422
levilianson <omaribarra27@gmail.com>
2022-04-14 12:58:07 -0300

    lista de items

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { TASKS } from 'src/app/mock-task';
 import { Task } from 'src/app/Task';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-task-item',
   templateUrl: './task-item.component.html',
@@ -9,6 +11,7 @@ import { Task } from 'src/app/Task';
 export class TaskItemComponent implements OnInit {
   //INICIALIZO CON LA PRIMER TAREA.
   @Input() task: Task = TASKS[0]
+  faTimes = faTimes;
   constructor() { }
 
   ngOnInit(): void {
--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { TASKS } from 'src/app/mock-task';
 import { Task } from 'src/app/Task';
 
+
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
